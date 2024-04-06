7d9578f48c59c9d2de40fc6b7ea0d412b8664d22
Vebjørn Nyvoll <vebjorn.nyvoll@gmail.com>
2024-04-06 13:49:02 +0200

    Fixed gap scoring, added better logging, fixed result printing

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from Bio import Align
 # This tool is designed to find the closest alignment to a query sequence in a fasta file
 # Use pip install -r requirements.txt to install required packages
 
+sequence_cap = 10000
+
 def read_fasta(filename):
     # Read fasta file and return dict of sequences
     sequences = {}
@@ -19,32 +21,44 @@ def find_closest_alignment(sequences, query_sequence):
     
     #stores aligment parameters
     aligner = Align.PairwiseAligner()
+    aligner.mode = 'global'
+    aligner.match_score = 2
+    aligner.mismatch_score = -2
+    aligner.open_gap_score = -0.5
+    aligner.extend_gap_score = -0.1
+
 
     best_alignment = []
     best_score = float("-inf")
     print("Finding best alignment, please wait...")
 
-    #find highest score
-    for identifier, sequence in sequences.items():
-
-        #calculte the alignment score
-        score = aligner.score(sequence, query_sequence)
-
-
-        if score > best_score:
-            best_score = score
-
-    #find alignment with highest score
-    for identifier, sequence in sequences.items():    
-        alignments = aligner.align(sequence, query_sequence) #Alignment objects represtenting alignments between sequence and query
-
-        for x in range(len(alignments)):
-            if alignments[x].score == best_score:
-                best_alignment.append(alignments[x].score)
     
 
+    #find alignment with highest score
+    counter = 0
+    for identifier, sequence in sequences.items():
+        try:
+            alignments = aligner.align(sequence, query_sequence) #Alignment objects represtenting alignments between sequence and query
+        except OverflowError: # Skip if alignment is too long
+            continue
+        # Create for loop counter and print
+        counter += 1
+        print("Aligning sequence", counter, "of", len(sequences), end="\r")
+
+        if 0 < len(alignments) < 50000: 
+            for x in range(len(alignments)):
+                if alignments[x].score >= best_score:
+                    # print(alignments[x])
+                    if alignments[x].score > best_score:
+                        best_score = alignments[x].score
+                        best_alignment = []
+                    best_alignment.append(alignments[x])
+                    # Only keep one best alignment from each sequence
+                    break
+        
+    print("\nAlignment complete.")
        
-    result = [len(best_alignment), best_score]
+    result = [best_alignment, best_score]
     return result
 # Main
 print("\n SVE Alignment Tool\n---------------------\n")
@@ -52,20 +66,23 @@ fasta_file = "uniprot_fasta/uniprot_sprot.fasta"
 print("Reading fasta file:", fasta_file, "please wait...")
 sequences = read_fasta(fasta_file)
 print("Number of Sequences:", len(sequences), "\n")
-if len(sequences) > 50000:
-    print('\x1b[1;31;40m' + "WARNING: More than 50k sequences! Recommended to use a subset for testing." + '\x1b[0m') # With all 500k sequences, the program can take several minutes to run
-    if input("Use first 50 sequences? (y/n): ").lower() == "y":
-        sequences = {k: sequences[k] for k in list(sequences)[:50]}
+if len(sequences) > sequence_cap:
+    print('\x1b[1;31;40m' + "WARNING: More than " + str(sequence_cap) + " sequences! Recommended to use a subset for testing." + '\x1b[0m') # With all 500k sequences, the program can take several minutes to run
+    if input("Use first " + str(sequence_cap) + " sequences? (y/n): ").lower() == "y":
+        sequences = {k: sequences[k] for k in list(sequences)[:sequence_cap]}
 print(len(sequences), "sequences will be used for alignment.")
 query_sequence = "REHSYWDSWSHKSMWYDDGCACPFGNNLHFHHPWANNYSCLTRIKFVIFM" # Example query sequence, can be replaced with input from user
 resultList = find_closest_alignment(sequences, query_sequence)
 
 
-print("Best Alignment:")
-#print("Identifier:", best_alignment[0])
-print("Sequence:")
-print(resultList[0])
+# Print results
+print("Best alignment: ")
+print(resultList[0][0])
+print("______________________________________")
+print("Number of alignments with best score: ")
+print(len(resultList[0]))
+print("______________________________________")
+print("Best alignment sequence ")
+print(resultList[0][0][0])
 
 print("Alignment Score:", resultList[1])
-#print("Alignment:")
-#print(pairwise2.format_alignment(*best_alignment[2]))
